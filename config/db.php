17ce286b2c8dcbdbcdf7807f86a90c2a2fd2c4d3
Frederick Jerusha <frederickjerusha@Fredericks-MacBook-Pro.local>
2025-05-06 16:24:19 +0700

    static

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=veristoretools2DB',
     'username' => 'user',
-    'password' => 'P@ssw0rd',
+    'password' => 'p@ssw0Rd',
     'charset' => 'utf8',
         // Schema cache options (for production environment)
     'enableSchemaCache' => true,
